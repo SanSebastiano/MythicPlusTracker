e4439b9f1b1aa3d7ce50a09bcbc890ff9368a713
SanSebastiano <sansebastiano@heroesofgamers.de>
2025-08-12 16:44:24 +0200

    fix: Great vault button frame affiliation

--- Modules/TrackerDashboard/GreatVaultButton.lua
+++ Modules/TrackerDashboard/GreatVaultButton.lua
@@ -2,7 +2,7 @@ local addonName, addon = ...
 
 MPT_GreatVaultButton = {}
 
-local function create()
+local function create(frame)
     local button = CreateFrame("Button", nil, frame)
     button:SetSize(54.62, 48)
     button:SetPoint("TOPRIGHT", frame, "TOPRIGHT", -30, -5)
@@ -15,7 +15,7 @@ local function create()
 end
 
 function MPT_GreatVaultButton:load(frame)
-    local button = create()
+    local button = create(frame)
 
     button:SetScript("OnEnter", function(self)
         GameTooltip:SetOwner(self, "ANCHOR_RIGHT")
