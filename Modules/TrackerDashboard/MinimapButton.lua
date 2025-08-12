3efdd4741f79476fa2035efeea9871e534f24b3c
SanSebastiano <sansebastiano@heroesofgamers.de>
2025-08-12 16:27:28 +0200

    Add minimap button

---
+++ Modules/TrackerDashboard/MinimapButton.lua
@@ -0,0 +1,49 @@
+local addonName, addon = ...
+
+MPT_MinimapButton = {}
+
+local function createMinimapButton()
+    local button = CreateFrame("Button", "MPTMinimapButton", Minimap)
+    button:SetSize(64, 64)
+    button:SetFrameStrata("MEDIUM")
+    button:SetFrameLevel(8)
+
+    local icon = button:CreateTexture(nil, "ARTWORK")
+    icon:SetAtlas("mythicplus-greatvault-collect")
+    icon:SetAllPoints()
+
+    local border = button:CreateTexture(nil, "OVERLAY")
+    border:SetAtlas("Minimap-TrackingBorder")
+    border:SetAllPoints()
+
+    button:SetPoint("BOTTOMRIGHT", Minimap, "BOTTOMRIGHT", 0, 0)
+
+    return button
+end
+
+function MPT_MinimapButton:load()
+    local button = createMinimapButton()
+
+    button:SetScript("OnClick", function(self, clickedButton)
+        if addon.isDebugMode then
+            print("MPT_MinimapButton clicked (" .. tostring(clickedButton) .. ")")
+        end
+        if clickedButton == "LeftButton" then
+            addon.trackerDashboard.frame:Show()
+        end
+    end)
+
+    button:SetScript("OnEnter", function(self)
+        GameTooltip:SetOwner(self, "ANCHOR_RIGHT")
+        GameTooltip:SetText("Mythic Plus Tracker Dashboard")
+        GameTooltip:AddLine("Click to toggle the dashboard.", 1, 1, 1)
+        GameTooltip:Show()
+    end)
+
+    button:SetScript("OnLeave", function()
+        GameTooltip:Hide()
+    end)
+
+    return button
+end
+
