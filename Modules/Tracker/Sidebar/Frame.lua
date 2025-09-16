d84f8e1f58883d4d2a082c4da4a545cecab223de
SanSebastiano <sansebastiano@heroesofgamers.de>
2025-09-16 14:52:57 +0200

    feat: Mythic plus core

--- Modules/Tracker/Sidebar/Frame.lua
+++ Modules/Tracker/Sidebar/Frame.lua
@@ -2,14 +2,15 @@ local addonName, addon = ...
 
 local function create(mainFrame)
     local frame = CreateFrame(
-            "Frame",
-            nil,
-            UIParent,
-            "BackdropTemplate"
+        "Frame",
+        nil,
+        mainFrame,
+        "BackdropTemplate"
     )
 
     frame:SetSize(300, 550)
     frame:SetPoint("TOPLEFT", mainFrame)
+
     frame:SetBackdrop({
         bgFile = "Interface\\DialogFrame\\UI-DialogBox-Background-Dark",
         edgeFile = "Interface\\Tooltips\\UI-Tooltip-Border",
@@ -19,15 +20,16 @@ local function create(mainFrame)
         edgeSize = 20,
         insets = { left = 5, right = 5, top = 5, bottom = 5 }
     })
-    frame:Hide()
 
     frame:SetScript("OnShow", function()
-        addon.successMessage("open")
+        MPT_Sidebar.getScore(frame)
     end)
 
     return frame
 end
 
-function MPT_Sidebar:getFrame(mainFrame)
-    return create(mainFrame)
+if MPT_Sidebar then
+    function MPT_Sidebar:getFrame(mainFrame)
+        return create(mainFrame)
+    end
 end
